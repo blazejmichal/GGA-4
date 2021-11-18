29d095a57332a3b9adc583f8c9134d3cea292bf6
blazej <blazejp.bp@gmail.com>
2021-11-18 19:27:26 +0100

    Po zamiance intow na sety.

--- src/main/java/model/Algorithm.java
+++ src/main/java/model/Algorithm.java
@@ -19,63 +19,87 @@ public class Algorithm {
     if (tree.get(root) != null) {
       return tree.get(root);
     }
-    int setWithoutCurrentRootSize = 0;
+//    int setWithoutCurrentRootSize = 0;
     Set<Node> setWithoutCurrentRoot = Sets.newLinkedHashSet();
     if (root.getLeft() != null) {
-      setWithoutCurrentRootSize = setWithoutCurrentRootSize + this.run(root.getLeft(), tree);
+//      setWithoutCurrentRootSize = setWithoutCurrentRootSize + this.run(root.getLeft(), tree);
+      this.run(root.getLeft(), tree);
       setWithoutCurrentRoot.addAll(
           root.getLeft().getIndependentSet()
       );
     }
     if (root.getRight() != null) {
-      setWithoutCurrentRootSize = setWithoutCurrentRootSize + this.run(root.getRight(), tree);
+//      setWithoutCurrentRootSize = setWithoutCurrentRootSize + this.run(root.getRight(), tree);
+      this.run(root.getRight(), tree);
       setWithoutCurrentRoot.addAll(
           root.getRight().getIndependentSet()
       );
     }
-    int setWithCurrentRootSize = 1;
+//    int setWithCurrentRootSize = 1;
     Set<Node> setWithCurrentRoot = Sets.newLinkedHashSet();
     setWithCurrentRoot.add(root);
     if (root.getLeft() != null) {
       if (root.getLeft().getLeft() != null) {
-        setWithCurrentRootSize += this.run(root.getLeft().getLeft(), tree);
+//        setWithCurrentRootSize += this.run(root.getLeft().getLeft(), tree);
+        this.run(root.getLeft().getLeft(), tree);
         setWithCurrentRoot.addAll(root.getLeft().getLeft().getIndependentSet());
       }
       if (root.getLeft().getRight() != null) {
-        setWithCurrentRootSize += this.run(root.getLeft().getRight(), tree);
+//        setWithCurrentRootSize += this.run(root.getLeft().getRight(), tree);
+        this.run(root.getLeft().getRight(), tree);
         setWithCurrentRoot.addAll(root.getLeft().getRight().getIndependentSet());
       }
     }
     if (root.getRight() != null) {
       if (root.getRight().getLeft() != null) {
-        setWithCurrentRootSize += this.run(root.getRight().getLeft(), tree);
+//        setWithCurrentRootSize += this.run(root.getRight().getLeft(), tree);
+        this.run(root.getRight().getLeft(), tree);
         setWithCurrentRoot.addAll(root.getRight().getLeft().getIndependentSet());
       }
       if (root.getRight().getRight() != null) {
-        setWithCurrentRootSize += this.run(root.getRight().getRight(), tree);
+//        setWithCurrentRootSize += this.run(root.getRight().getRight(), tree);
+        this.run(root.getRight().getRight(), tree);
         setWithCurrentRoot.addAll(root.getRight().getRight().getIndependentSet());
       }
     }
     setWithoutCurrentRoot.removeAll(Collections.singleton(null));
-    if (setWithCurrentRootSize != setWithCurrentRoot.size()
-        || setWithoutCurrentRootSize
-        != setWithoutCurrentRoot.size()
-    ) {
-      System.out.println();
-    }
-    if (setWithCurrentRootSize > setWithoutCurrentRootSize) {
+//    if (setWithCurrentRootSize != setWithCurrentRoot.size()
+//        || setWithoutCurrentRootSize
+//        != setWithoutCurrentRoot.size()
+//    ) {
+//      System.out.println();
+//    }
+    if (setWithCurrentRoot.size() > setWithoutCurrentRoot.size()) {
       root.addToIndependentSet(
           setWithCurrentRoot
       );
       root.getIndependentSet().add(root);
+      tree.put(
+          root,
+          setWithCurrentRoot.size()
+      );
     } else {
       root.addToIndependentSet(
           setWithoutCurrentRoot
       );
+      tree.put(
+          root,
+          setWithoutCurrentRoot.size()
+      );
     }
-    tree.put(root, Integer.max(setWithoutCurrentRootSize,
-        setWithCurrentRootSize));
+//    tree.put(root, Integer.max(setWithoutCurrentRootSize,
+//        setWithCurrentRootSize));
     return tree.get(root);
+  }
+
+  public void findSetWithRoot(
+      Node root
+  ) {
+  }
+
+  public void findSetWithoutRoot(
+      Node root
+  ) {
   }
 
 }
