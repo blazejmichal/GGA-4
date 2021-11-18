b53d7a5a7503c95794171f939972a9c0322360fc
blazej <blazejp.bp@gmail.com>
2021-11-18 19:58:53 +0100

    Rozbicie na metody z orz bez

--- src/main/java/model/Algorithm.java
+++ src/main/java/model/Algorithm.java
@@ -2,114 +2,89 @@ package model;
 
 import com.google.common.collect.Sets;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Set;
 
 public class Algorithm {
 
-//  public int run(
   public void run(
       Node root
-//      Map<Node, Integer> tree
   ) {
-//    // Obsługa gdy dany korzeń od którego liczymy jest nullem
-//    if (root == null) {
-//      return 0;
-//    }
     // Jeśli dany korzeń był już przeliczany to ta wartość zostanie przywołana
-//    if (tree.get(root) != null) {
-//      return tree.get(root);
-//    }
     if (root.getIndependentSet().size() != 0) {
       return;
     }
-//    int setWithoutCurrentRootSize = 0;
-    Set<Node> setWithoutCurrentRoot = Sets.newLinkedHashSet();
-    if (root.getLeft() != null) {
-//      setWithoutCurrentRootSize = setWithoutCurrentRootSize + this.run(root.getLeft(), tree);
-//      this.run(root.getLeft(), tree);
-      this.run(root.getLeft());
-      setWithoutCurrentRoot.addAll(
-          root.getLeft().getIndependentSet()
-      );
-    }
-    if (root.getRight() != null) {
-//      setWithoutCurrentRootSize = setWithoutCurrentRootSize + this.run(root.getRight(), tree);
-//      this.run(root.getRight(), tree);
-      this.run(root.getRight());
-      setWithoutCurrentRoot.addAll(
-          root.getRight().getIndependentSet()
-      );
-    }
-//    int setWithCurrentRootSize = 1;
+    Set<Node> setWithoutCurrentRoot = this.findSetWithoutRoot(root);
+    Set<Node> setWithCurrentRoot = this.findSetWithRoot(root);
+    this.attachIndependentSetToRoot(
+        root,
+        setWithoutCurrentRoot,
+        setWithCurrentRoot
+    );
+  }
+
+  public Set<Node> findSetWithRoot(
+      Node root
+  ) {
     Set<Node> setWithCurrentRoot = Sets.newLinkedHashSet();
     setWithCurrentRoot.add(root);
     if (root.getLeft() != null) {
       if (root.getLeft().getLeft() != null) {
-//        setWithCurrentRootSize += this.run(root.getLeft().getLeft(), tree);
-//        this.run(root.getLeft().getLeft(), tree);
         this.run(root.getLeft().getLeft());
         setWithCurrentRoot.addAll(root.getLeft().getLeft().getIndependentSet());
       }
       if (root.getLeft().getRight() != null) {
-//        setWithCurrentRootSize += this.run(root.getLeft().getRight(), tree);
-//        this.run(root.getLeft().getRight(), tree);
         this.run(root.getLeft().getRight());
         setWithCurrentRoot.addAll(root.getLeft().getRight().getIndependentSet());
       }
     }
     if (root.getRight() != null) {
       if (root.getRight().getLeft() != null) {
-//        setWithCurrentRootSize += this.run(root.getRight().getLeft(), tree);
-//        this.run(root.getRight().getLeft(), tree);
         this.run(root.getRight().getLeft());
         setWithCurrentRoot.addAll(root.getRight().getLeft().getIndependentSet());
       }
       if (root.getRight().getRight() != null) {
-//        setWithCurrentRootSize += this.run(root.getRight().getRight(), tree);
-//        this.run(root.getRight().getRight(), tree);
         this.run(root.getRight().getRight());
         setWithCurrentRoot.addAll(root.getRight().getRight().getIndependentSet());
       }
     }
+    return setWithCurrentRoot;
+  }
+
+  public Set<Node> findSetWithoutRoot(
+      Node root
+  ) {
+    Set<Node> setWithoutCurrentRoot = Sets.newLinkedHashSet();
+    if (root.getLeft() != null) {
+      this.run(root.getLeft());
+      setWithoutCurrentRoot.addAll(
+          root.getLeft().getIndependentSet()
+      );
+    }
+    if (root.getRight() != null) {
+      this.run(root.getRight());
+      setWithoutCurrentRoot.addAll(
+          root.getRight().getIndependentSet()
+      );
+    }
     setWithoutCurrentRoot.removeAll(Collections.singleton(null));
-//    if (setWithCurrentRootSize != setWithCurrentRoot.size()
-//        || setWithoutCurrentRootSize
-//        != setWithoutCurrentRoot.size()
-//    ) {
-//      System.out.println();
-//    }
-    if (setWithCurrentRoot.size() > setWithoutCurrentRoot.size()) {
+    return setWithoutCurrentRoot;
+  }
+
+  public void attachIndependentSetToRoot(
+      Node root,
+      Set<Node> setWithoutCurrentRoot,
+      Set<Node> setWithCurrentRoot
+  ) {
+    if (setWithoutCurrentRoot.size() < setWithCurrentRoot.size()) {
       root.addToIndependentSet(
           setWithCurrentRoot
       );
       root.getIndependentSet().add(root);
-//      tree.put(
-//          root,
-//          setWithCurrentRoot.size()
-//      );
     } else {
       root.addToIndependentSet(
           setWithoutCurrentRoot
       );
-//      tree.put(
-//          root,
-//          setWithoutCurrentRoot.size()
-//      );
     }
-//    tree.put(root, Integer.max(setWithoutCurrentRootSize,
-//        setWithCurrentRootSize));
-//    return tree.get(root);
-  }
-
-  public void findSetWithRoot(
-      Node root
-  ) {
-  }
-
-  public void findSetWithoutRoot(
-      Node root
-  ) {
   }
 
 }
